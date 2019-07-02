7d12073ba89a1a2a9f91552af4b98016141e42c1
faithngotho <faithwangari248@gmail.com>
2019-07-02 12:14:01 +0300

    add imported render_template,request,redirect,url_for

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from app import app
-
+from flask import render_template,request,redirect,url_for
 # Views
 @app.route('/')
 def index():
@@ -25,3 +25,14 @@ def pitch(id):
     title = f'{pitch.title}'
 
     return render_template('pitch.html',title = title,pitch = pitch)
+
+@app.route('/search/<pitch_name>')
+def search(pitch_name):
+    '''
+    View function to display the search results
+    '''
+    pitch_name_list = pitch_name.split(" ")
+    pitch_name_format = "+".join(pitch_name_list)
+    searched_pitch = search_pitch(pitch_name_format)
+    title = f'search results for {pitch_name}'
+    return render_template('search.html',pitch = searched_pitch)
