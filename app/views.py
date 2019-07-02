4c504872ed0d9e63cff548eca590207cc60debb6
faithngotho <faithwangari248@gmail.com>
2019-07-02 11:16:47 +0300

    add added code on the pitch.html

--- app/views.py
+++ app/views.py
@@ -13,3 +13,15 @@ def index():
     print(pitch)
     title = 'Home - Welcome to Pitch Website Online'
     return render_template('index.html', title = title,popular = Pitch)
+
+
+@app.route('/pitch/<int:id>')
+def pitch(id):
+
+    '''
+    View pitch page function that returns the pitch details page and its data
+    '''
+    pitch = get_pitch(id)
+    title = f'{pitch.title}'
+
+    return render_template('pitch.html',title = title,pitch = pitch)
