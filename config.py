2749b7b4ef0f5cf1eb3c6b5f10367e9d5b784f82
faithngotho <faithwangari248@gmail.com>
2019-07-03 09:21:17 +0300

    add added code to the welcome_user.txt

--- config.py
+++ config.py
@@ -1,24 +1,49 @@
-
 import os
-class Config:
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
-    SECRET_KEY = os.environ.get('SECRET_KEY') 
-    WTF_CSRF_SECRET_KEY="a csrf secret key"
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://faith:34816809@localhost/pitch'
 
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SECRET_KEY = "1234"
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    
+    SSL_DISABLE = False
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLCLCHEMY_RECORD_QUERIES = True
+    CSRF_ENABLED = True
+    MAIL_SERVER = os.environ.get('MAIL_SERVER','smtp.gmail.com')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME','faithwangari248@gmail.com')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD','generari254')
+    MAIL_SENDER = os.environ.get('MAIL_SENDER', 'Project Admin<faithwangari248@gmail.com>')
+    PROJECT_ADMIN = os.environ.get('PROJECT_ADMIN', 'PROJECT_ADMIN')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', '465'))
+    MAIL_USE_TLS = int(os.environ.get('MAIL_USE_TLS', False))
+    MAIL_USE_SSL = int(os.environ.get('MAIL_USE_SSL',  True))
+    MAIL_SUBJECT_PREFIX = '[PROJECT]'
+
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
 
 class ProdConfig(Config):
-  SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-
+    '''
+    Production configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://faith:34816809@localhost/pitch'
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://faith:34816809@localhost/pitches'
+
     DEBUG = True
-class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://faith:34816809@localhost/pitch'
-    
+
 config_options = {
 'development':DevConfig,
-'production':ProdConfig,
-'test':TestConfig
+'production':ProdConfig
 }
