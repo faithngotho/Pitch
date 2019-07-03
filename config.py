bb15cdb84221227805ba353e56e2077e2c0a8fbf
faithngotho <faithwangari248@gmail.com>
2019-07-03 15:17:54 +0300

    deployment

--- config.py
+++ config.py
@@ -17,8 +17,8 @@ class Config:
     MAIL_SENDER = os.environ.get('MAIL_SENDER', 'Project Admin<faithwangari248@gmail.com>')
     PROJECT_ADMIN = os.environ.get('PROJECT_ADMIN', 'PROJECT_ADMIN')
     MAIL_PORT = int(os.environ.get('MAIL_PORT', '465'))
-    MAIL_USE_TLS = int(os.environ.get('MAIL_USE_TLS', False))
-    MAIL_USE_SSL = int(os.environ.get('MAIL_USE_SSL',  True))
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
     MAIL_SUBJECT_PREFIX = '[PROJECT]'
 
     # simple mde  configurations
